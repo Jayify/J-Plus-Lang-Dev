f9953dae9bccdf59ff5a54c95641de8d031c58e9
Jayden <jayden.hou12@gmail.com>
2022-04-15 15:26:08 +1200

    V0.3.1 - Improved error message with colour and reword

--- shell.py
+++ shell.py
@@ -2,9 +2,9 @@ import j_plus as jp
 
 while True:
     text = input("J+ stdin> ")
-    result, error = jp.run("<stdin>", text)
+    result, error = jp.run("<stdin terminal>", text)
 
     if error:
-        print(error.as_string())
+        print(f'\033[0;31mERROR: {error.as_string()} \033[0;0m')
     else:
         print(result)
